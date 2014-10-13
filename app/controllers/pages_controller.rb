4b700d444e88d0609f46241ea630af2deb2e1433
AndyNhi <nhi.atn@gmail.com>
2014-10-13 10:48:02 -0600

    Converted Boxes to Hash Containment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
 
 def index
+  @columns = {
+    Task: ['Group Project and list just the way you like them.'],
+    Documents: ['Upload', 'Comment', 'Revise'],
+    Comments: ['Comments on task and documents','Get email notifications']
+    }
+    
+  render :index
+
+
 end
 
 def task
