09fd3ee8b00e348e25e808aa7057b85c7e026441
AndyNhi <nhi.atn@gmail.com>
2014-10-14 13:51:12 -0600

    Added Quotes to Index Page (Array Based)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,16 @@ def index
     Documents: ['Upload', 'Comment', 'Revise'],
     Comments: ['Comments on task and documents','Get email notifications']
     }
+
+  @quote_author_array = [
+    ['Tupac',['Death is not the greatest loss in life. The greatest loss is what dies inside while still alive. Never surrender.']],
+    ['Albert Einstein',['The difference between stupidity and genius is that genius has its limits.']],
+    ['George Carlin',['Those who dance are considered insane by those who cannot hear the music.']]
+    ]
+    
   render :index
 end
+
+
 
 end
