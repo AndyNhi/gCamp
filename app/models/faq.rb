7a63cdae500fa1890d432e9f5e41bf046b5660c5
AndyNhi <nhi.atn@gmail.com>
2014-10-15 15:54:04 -0600

    Intialize and methods all stretch

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -2,4 +2,12 @@ class Faq
 
   attr_accessor :faq_question, :faq_answer
 
+  def initialize
+    @faq_questions_test = "This is an initialize test and method call on all!"
+  end
+
+  def all
+    @faq_questions_test
+  end
+
 end
