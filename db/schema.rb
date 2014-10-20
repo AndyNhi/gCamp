2496b4dc359bd7e713e44829502a0490ded80259
AndyNhi <nhi.atn@gmail.com>
2014-10-20 16:20:14 -0600

    due date and check_box migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016200346) do
+ActiveRecord::Schema.define(version: 20141020205626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20141016200346) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "complete"
+    t.datetime "due_date"
   end
 
 end
