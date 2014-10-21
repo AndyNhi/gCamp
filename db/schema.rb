dee6f3ee83629231b45c89b03fbb8ebda8cf7b88
AndyNhi <nhi.atn@gmail.com>
2014-10-21 10:35:24 -0600

    change migration remove title

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020205626) do
+ActiveRecord::Schema.define(version: 20141021162238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: true do |t|
-    t.string   "title"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
