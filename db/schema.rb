249bf9d61c87beb1eb216e3345a1236c055ccb03
AndyNhi <nhi.atn@gmail.com>
2014-10-22 15:48:10 -0600

    change faq to active model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021162238) do
+ActiveRecord::Schema.define(version: 20141022211101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "faqs", force: true do |t|
+    t.string "question"
+    t.text   "answer"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "description"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+faq1 = Faq.create(
+                  :question => 'What is gCamp?',
+                  :answer   => 'Gcamp is an awesome tool that is going to change your life
+                                Gcamp is your one stop shop to organize all your task and
+                                documents. Youll also be able to track comments that you and
+                                other makes. gCamp may eventually replace all need for paper
+                                and pens in the entire world. Well, maybe not, but its going
+                                to be pretty cool'
+                 )
+
+faq2 = Faq.create(
+                  :question => 'How do I join gCamp?',
+                  :answer   => 'Right now gCamp is still in production. So there is not a
+                                sign up page open to the public, yet! Your best options it
+                                to become super best friends with gCamp developer. They can
+                                be found in the hanging around gSchool during the day and
+                                hitting the hottest clubs at night.'
+                 )
+
+faq3 = Faq.create(
+                  :question => 'When will gCamp be finished?',
+                  :answer   => 'gCamp is work in progress. That being said, it should be
+                                fully functional by Deceember 2014. Functional, but our
+                                developers are going to continue to improve the sight for the
+                                foreseeable future. Check in daily for new features and awesome
+                                functionality. Its its going to blow your mind.Organization is only
+                                (well, will only). be click away. Amazing'
+                 )
