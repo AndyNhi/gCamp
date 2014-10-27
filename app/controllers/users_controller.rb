b5a1f5732e8b1f99449991b81033d425661a9bdd
AndyNhi <nhi.atn@gmail.com>
2014-10-27 14:33:31 -0600

    created users manually crud

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def index
+      @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def edit
+    end
+
+    def create
+      @user = User.new(params.require(:user).permit(:first_name, :last_name, :email_address))
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to @user, notice: 'Task was successfully created.'}
+          format.json { render :show, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'Task was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'Task was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026194425) do
+ActiveRecord::Schema.define(version: 20141027193218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20141026194425) do
     t.datetime "updated_at"
     t.boolean  "complete",    default: false, null: false
     t.datetime "due_date"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
