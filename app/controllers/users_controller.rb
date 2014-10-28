3c0139a17ba2058094e1524a8c5ad7f161ab9479
AndyNhi <nhi.atn@gmail.com>
2014-10-28 14:41:37 -0600

    add highlight

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+
     def index
       @users = User.all
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ActiveRecord::Base
+
+  def due_soon?
+    if self.due_date
+      true if self.due_date <= (Date.today + 7)
+    else
+      nil
+    end
+  end
+
+
 end
