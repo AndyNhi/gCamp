7b0b11838f76c704b985d29f37a4a37e4521b2cc
AndyNhi <nhi.atn@gmail.com>
2014-10-29 17:41:30 -0600

    projects crud

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,40 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params.require(:project).permit(:description))
+    @project.save
+    redirect_to projects_path
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(params.require(:project).permit(:description))
+    redirect_to projects_path
+  end
+
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_path, notice: 'Task was successfully destroyed.' }
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027193218) do
+ActiveRecord::Schema.define(version: 20141029222232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20141027193218) do
   end
 
   create_table "pages", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
