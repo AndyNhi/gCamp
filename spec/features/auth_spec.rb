d23955dd24e326add28a1d79fcf3320f8016b53f
AndyNhi <nhi.atn@gmail.com>
2014-11-05 16:30:23 -0700

    added user and task crud testing

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+feature "Authentication" do
+
+  def login
+    User.create(first_name: 'a', last_name: 'a', email_address: 'a@email.com', password: 'a')
+    visit root_path
+    click_link('Sign In')
+    visit '/sign-in'
+    fill_in 'Email', :with => 'a@email.com'
+    fill_in 'Password', :with => 'a'
+    click_button 'Log In'
+    expect(page).to have_content 'Welcome'
+  end
+
+  scenario "User can sign up" do
+
+    visit root_path
+    click_link('Sign Up')
+    visit '/sign-up'
+    fill_in 'First name', with: 'test'
+    fill_in 'Last name', with: 'test'
+    fill_in 'Email address', with: 'test@email.com'
+    fill_in 'Password', with: 'test'
+    fill_in 'Password confirmation', with: :password
+    click_button 'Create User'
+    expect(page).to have_content :notice
+
+  end
+
+  scenario "User signs into a session" do
+
+    login
+
+  end
+
+  scenario "User signs out of session" do
+
+    login
+    click_link('Sign Out')
+    expect(page).to have_content 'Sign In'
+
+  end
+
+end
---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+
+feature "Task Crud" do
+
+  def create_task
+    visit "/tasks"
+    click_link "Create Task"
+    fill_in "Description", with: "test1"
+    fill_in "Due date", with: "25/01/2014"
+    click_button "Create Task"
+    expect(page).to have_content "test1"
+    visit "/tasks"
+  end
+
+
+  scenario "create tasks" do
+    create_task
+  end
+
+
+  scenario "read tasks" do
+    create_task
+    visit "/tasks"
+    click_link "Show"
+    expect(page).to have_content "test1"
+  end
+
+
+  scenario "update tasks" do
+    create_task
+    visit "/tasks"
+    click_link "Edit"
+    fill_in "Description", with: "Andy"
+    click_button "Update Task"
+    expect(page).to have_content "Andy"
+  end
+
+
+  scenario "destroy tasks" do
+    create_task
+    visit "/tasks"
+    click_link "Destroy"
+    expect(page).to have_no_content "test1"
+  end
+
+
+end
