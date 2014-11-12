9a3cd49ef50c59b30eb2fe0cc43b3aa6eb7985cc
AndyNhi <nhi.atn@gmail.com>
2014-11-12 14:59:13 -0700

    add update test for date

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -8,6 +8,12 @@ describe Task do
                         description: "description",
                         complete: false,
                         due_date: '01/01/1999')
+
+    @task_update = Task.create(
+                                title: "title",
+                                description: "description",
+                                complete: false,
+                                due_date: '01/01/1999')
   end
 
     describe '#create' do
@@ -18,7 +24,23 @@ describe Task do
         @task.due_date = '01/01/2999'
         @task.valid?
         expect(@task.errors[:due_date].present?).to be(false)
+      end
+
+
+    describe '#update' do
+      it "should be able to update completed task to any date" do
+
+        @task_update.update(:due_date => '01/01/2999')
+        @task_update.valid?
+        expect(@task_update.errors[:due_date].present?).to be(false)
+
+        @task_update.update(:due_date => '01/01/1999')
+        @task_update.valid?
+        expect(@task_update.errors[:due_date].present?).to be(false)
+      end
     end
+
+
   end
 
 
