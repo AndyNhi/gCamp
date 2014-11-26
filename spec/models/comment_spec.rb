30d5e12324e43521e1a3f305328886a95e736356
AndyNhi <nhi.atn@gmail.com>
2014-11-25 21:12:49 -0700

    change rails helper to shorten factory call and modified test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Comment' do
 
   before(:each) do
-    project = FactoryGirl.create(:project)
-    @user = FactoryGirl.create(:user)
-    task = FactoryGirl.create(:task, due_date: '01/01/2999', project_id: project.id)
-    @comment = FactoryGirl.build(:comment, user_id: @user.id, task_id: task.id)
+    project = create(:project)
+    @user = create(:user)
+    task = create(:task, due_date: '01/01/2999', project_id: project.id)
+    @comment = build(:comment, user_id: @user.id, task_id: task.id)
   end
 
   it 'is valid with a user' do
