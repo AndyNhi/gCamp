66ebaf48c474be4db3e62f6f3e7d411a5421e337
AndyNhi <nhi.atn@gmail.com>
2014-11-29 21:11:51 -0700

    pluralize function

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,9 +2,6 @@ class Membership < ActiveRecord::Base
 
   belongs_to :project
   belongs_to :user
-  # def user
-  #   User.find(self.user_id)
-  # end
 
   validates :user, uniqueness: {scope: :project, message: "has already been added"}
   validates_presence_of :user
