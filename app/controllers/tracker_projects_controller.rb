18fa4a8e2da86e8af6b63c1cdf4b63f01bc55b4b
AndyNhi <nhi.atn@gmail.com>
2014-12-10 22:11:49 -0700

    change format of commas

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -8,6 +8,8 @@ class TrackerProjectsController < ApplicationController
       @pivotol_stories_per_project = tracker_api.stories(project_id, current_user.pivotal_tracker_token)
       @pivotal_stories_project_name = tracker_api.project(project_id, current_user.pivotal_tracker_token)
     end
+
+
   end
 
 
