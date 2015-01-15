f3771fb1e7a5a60b60e8fff4b4cbfd89cea0b1ee
AndyNhi <nhi.atn@gmail.com>
2015-01-15 11:12:50 -0700

    fixed unless

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -3,9 +3,10 @@ class TrackerProjectsController < ApplicationController
   def show
     project_id = params[:id]
     tracker_api = TrackerProject.new
-    - unless current_user.pivotal_tracker_token.nil?
+    unless current_user.pivotal_tracker_token.nil?
       @pivotol_stories_per_project = tracker_api.stories(project_id, current_user.pivotal_tracker_token)
       @pivotal_stories_project_name = tracker_api.project(project_id, current_user.pivotal_tracker_token)
+    end
   end
 
 
